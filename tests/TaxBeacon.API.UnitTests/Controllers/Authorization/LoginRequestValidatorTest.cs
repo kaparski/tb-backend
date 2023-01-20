5d6b8beb60aa8287aeb7a8d5756b19b75404eb3b
Alexander Makar <alexandr.makar@kandasoft.com>
2023-01-20 15:42:56 +0300

    Added unit tests

---
+++ tests/TaxBeacon.API.UnitTests/Controllers/Authorization/LoginRequestValidatorTest.cs
@@ -0,0 +1,57 @@
+﻿using Bogus;
+using FluentValidation.TestHelper;
+using TaxBeacon.API.Controllers.Authorization;
+
+namespace TaxBeacon.API.UnitTests.Controllers.Authorization;
+
+public class LoginRequestValidatorTest
+{
+    private readonly LoginRequestValidator _loginRequestValidator;
+
+    public LoginRequestValidatorTest() => _loginRequestValidator = new LoginRequestValidator();
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData("1")]
+    public void Validation_InvalidEmail_ShouldHaveErrors(string email)
+    {
+        //Arrange
+        var loginRequest = new LoginRequest(email);
+
+        //Act
+        var actualResult = _loginRequestValidator.TestValidate(loginRequest);
+
+        //Assert
+        actualResult.ShouldHaveValidationErrorFor(r => r.Email);
+    }
+
+    [Fact]
+    public void Validation_ValidEmail_ShouldHaveNoErrors()
+    {
+        //Arrange
+        var loginRequest = new LoginRequest(new Faker().Internet.Email());
+
+        //Act
+        var actualResult = _loginRequestValidator.TestValidate(loginRequest);
+
+        //Assert
+        actualResult.ShouldNotHaveValidationErrorFor(r => r.Email);
+    }
+
+    [Fact]
+    public void Validation_LongEmail_ShouldHavMxLengthError()
+    {
+        //Arrange
+        var email = $"{new Faker().Random.String2(200)}@email.com";
+        var loginRequest = new LoginRequest(email);
+
+        //Act
+        var actualResult = _loginRequestValidator.TestValidate(loginRequest);
+
+        //Assert
+        actualResult
+            .ShouldHaveValidationErrorFor(r => r.Email)
+            .WithErrorMessage("The email must contain no more than 150 characters");
+    }
+}
---
+++ tests/TaxBeacon.UserManagement.UnitTests/Services/UserServiceTests.cs
@@ -0,0 +1,109 @@
+﻿using Bogus;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Moq;
+using TaxBeacon.Common.Enums;
+using TaxBeacon.DAL;
+using TaxBeacon.DAL.Entities;
+using TaxBeacon.DAL.Interceptors;
+using TaxBeacon.DAL.Interfaces;
+using TaxBeacon.UserManagement.Services;
+
+namespace TaxBeacon.UserManagement.UnitTests.Services;
+
+public class UserServiceTests
+{
+    private readonly Mock<ILogger<UserService>> _userServiceLoggerMock;
+    private readonly Mock<EntitySaveChangesInterceptor> _entitySaveChangesInterceptorMock;
+    private readonly ITaxBeaconDbContext _dbContextMock;
+    private readonly UserService _userService;
+
+    public UserServiceTests()
+    {
+        _userServiceLoggerMock = new();
+        _entitySaveChangesInterceptorMock = new();
+
+        _dbContextMock = new TaxBeaconDbContext(
+            new DbContextOptionsBuilder<TaxBeaconDbContext>()
+                .UseInMemoryDatabase($"{nameof(UserServiceTests)}-InMemoryDb-{Guid.NewGuid()}")
+                .Options,
+            _entitySaveChangesInterceptorMock.Object);
+
+        _userService = new UserService(_userServiceLoggerMock.Object, _dbContextMock);
+    }
+
+    [Fact]
+    public async Task LoginAsync_ValidEmailAndUserExist_LastLoginDateUpdated()
+    {
+        //Arrange
+        var tenant = TestData.TestTenant.Generate();
+        var user = TestData.TestUser.Generate();
+        var currentDate = DateTime.UtcNow;
+
+        user.TenantUsers.Add(new() { Tenant = tenant });
+        await _dbContextMock.Tenants.AddAsync(tenant);
+        await _dbContextMock.Users.AddAsync(user);
+        await _dbContextMock.SaveChangesAsync();
+
+        //Act
+        await _userService.LoginAsync(user.Email);
+        var actualResult = await _dbContextMock.Users.LastAsync();
+
+        //Assert
+        (await _dbContextMock.SaveChangesAsync()).Should().Be(0);
+        actualResult.LastLoginDateUtc.Should().BeAfter(currentDate);
+    }
+
+    [Fact]
+    public async Task LoginAsync_ValidEmailAndUserNotExist_NewUserCreated()
+    {
+        //Arrange
+        var tenant = TestData.TestTenant.Generate();
+        var user = TestData.TestUser.Generate();
+        var currentDate = DateTime.UtcNow;
+
+        _dbContextMock.Tenants.Add(tenant);
+        await _dbContextMock.SaveChangesAsync();
+
+        //Act
+        await _userService.LoginAsync(user.Email);
+        var actualResult = await _dbContextMock.Users.LastAsync();
+
+        //Assert
+        using (new AssertionScope())
+        {
+            (await _dbContextMock.SaveChangesAsync()).Should().Be(0);
+            actualResult.Email.Should().Be(user.Email);
+            actualResult.LastName.Should().BeEmpty();
+            actualResult.FirstName.Should().BeEmpty();
+            actualResult.Username.Should().BeEmpty();
+            actualResult.TenantUsers.Should()
+                .NotBeEmpty()
+                .And
+                .HaveCount(1);
+            actualResult.TenantUsers.First().TenantId.Should().Be(tenant.Id);
+            actualResult.LastLoginDateUtc.Should().BeAfter(currentDate);
+        }
+    }
+
+    private static class TestData
+    {
+        public static readonly Faker<Tenant> TestTenant =
+            new Faker<Tenant>()
+                .RuleFor(t => t.Id, f => Guid.NewGuid())
+                .RuleFor(t => t.Name, f => f.Company.CompanyName())
+                .RuleFor(t => t.CreatedDateUtc, f => DateTime.UtcNow);
+
+        public static readonly Faker<User> TestUser =
+            new Faker<User>()
+                .RuleFor(u => u.Id, f => Guid.NewGuid())
+                .RuleFor(u => u.FirstName, f => f.Name.FirstName())
+                .RuleFor(u => u.LastName, f => f.Name.LastName())
+                .RuleFor(u => u.Username, (f, u) => f.Internet.UserName(u.FirstName, u.LastName))
+                .RuleFor(u => u.Email, f => f.Internet.Email())
+                .RuleFor(u => u.CreatedDateUtc, f => DateTime.UtcNow)
+                .RuleFor(u => u.UserStatus, f => f.PickRandom<UserStatus>());
+    }
+}
