80964f147db9ae84b5e76d4e96bc018e5a221eed
kanda-dosya <dastan.alybaev@kandasoft.com>
2023-02-17 15:58:27 +0600

    refactor: refactor code

--- src/TaxBeacon.API/Exceptions/CustomProblemDetails.cs
+++ src/TaxBeacon.API/Exceptions/CustomProblemDetails.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json.Serialization;
 
 namespace TaxBeacon.API.Exceptions;
 
 public class CustomProblemDetails: ProblemDetails
 {
+    [JsonPropertyName("errorCode")]
     public int ErrorCode { get; set; }
 }
