a93f349b3967d9fd603a87900434645f25640269
Alexander Makar <alexandr.makar@kandasoft.com>
2023-02-24 12:50:21 +0300

    Adjusted validation for CreateUserRequest.cs

--- src/TaxBeacon.API/Controllers/Authorization/Requests/LoginRequest.cs
+++ src/TaxBeacon.API/Controllers/Authorization/Requests/LoginRequest.cs
@@ -1,6 +1,6 @@
 ﻿using FluentValidation;
 
-namespace TaxBeacon.API.Controllers.Authorization;
+namespace TaxBeacon.API.Controllers.Authorization.Requests;
 
 public record LoginRequest(string Email);
 
@@ -8,9 +8,8 @@ public class LoginRequestValidator: AbstractValidator<LoginRequest>
 {
     public LoginRequestValidator() =>
         RuleFor(x => x.Email)
+            .NotEmpty()
             .EmailAddress()
             .MaximumLength(150)
-            .WithMessage("The email must contain no more than 150 characters")
-            .NotNull()
-            .NotEmpty();
+            .WithMessage("The email must contain no more than 150 characters");
 }
