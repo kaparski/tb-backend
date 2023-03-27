b637d3396cd77b5127f1ab837deb755e70d73fca
kanda-dosya <dastan.alybaev@kandasoft.com>
2023-03-27 14:24:45 +0600

    Added tabulation

--- migration-scripts/InsertPermissionsAndRoles.sql
+++ migration-scripts/InsertPermissionsAndRoles.sql
@@ -8,36 +8,36 @@ DECLARE @RoleId AS UNIQUEIDENTIFIER = NEWID()
 DECLARE @RoleName AS NVARCHAR(100) = N'Admin'
 
 BEGIN TRANSACTION [Tran1];
-BEGIN TRY
-IF NOT EXISTS(SELECT Id FROM Roles WHERE Id = @RoleId)
-BEGIN
-INSERT INTO Roles(Id, Name, CreatedDateUtc) VALUES (@RoleId, @RoleName, GETUTCDATE());
-END;
+  BEGIN TRY
+    IF NOT EXISTS(SELECT Id FROM Roles WHERE Id = @RoleId)
+  BEGIN
+    INSERT INTO Roles(Id, Name, CreatedDateUtc) VALUES (@RoleId, @RoleName, GETUTCDATE());
+  END;
 
   IF NOT EXISTS(SELECT RoleId FROM TenantRoles WHERE RoleId = @RoleId AND TenantId = @TenantId)
-BEGIN
-INSERT INTO TenantRoles VALUES (@TenantId, @RoleId);
-END;
+  BEGIN
+    INSERT INTO TenantRoles VALUES (@TenantId, @RoleId);
+  END;
 
-INSERT INTO TenantUsers
-SELECT Id, @TenantId
-FROM Users AS u
-WHERE NOT EXISTS(SELECT UserId
-                 FROM TenantUsers
-                 WHERE UserId = u.Id
-                   AND TenantId = @TenantId);
+  INSERT INTO TenantUsers
+  SELECT Id, @TenantId
+  FROM Users AS u
+  WHERE NOT EXISTS(SELECT UserId
+                   FROM TenantUsers
+                   WHERE UserId = u.Id
+                     AND TenantId = @TenantId);
 
-INSERT INTO TenantUserRoles
-SELECT TenantId, @RoleId, UserId
-FROM TenantUsers AS tu
-WHERE NOT EXISTS(SELECT UserId
-                 FROM TenantUserRoles
-                 WHERE UserId = tu.UserId
-                   AND TenantId = tu.TenantId
-                   AND RoleId = @RoleId);
+  INSERT INTO TenantUserRoles
+  SELECT TenantId, @RoleId, UserId
+  FROM TenantUsers AS tu
+  WHERE NOT EXISTS(SELECT UserId
+                   FROM TenantUserRoles
+                   WHERE UserId = tu.UserId
+                     AND TenantId = tu.TenantId
+                     AND RoleId = @RoleId);
 
-COMMIT TRANSACTION [Tran1];
-END TRY
+    COMMIT TRANSACTION [Tran1];
+  END TRY
 BEGIN CATCH
-ROLLBACK TRANSACTION [Tran1]
+  ROLLBACK TRANSACTION [Tran1]
 END CATCH
