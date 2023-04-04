6eae7c01fe118068a44fef513cdae0e6b832b32a
Konstantin <129409014+m7vqqcl0@users.noreply.github.com>
2023-04-04 08:50:09 +0200

    more tests

---
+++ tests/TaxBeacon.API.UnitTests/Controllers/Tenant/TenantsControllerTest.cs
@@ -0,0 +1,88 @@
+﻿using Bogus;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using Gridify;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System.Security.Claims;
+using TaxBeacon.API.Authentication;
+using TaxBeacon.API.Controllers.Tenants;
+using TaxBeacon.API.Controllers.Tenants.Responses;
+using TaxBeacon.API.Controllers.Users;
+using TaxBeacon.API.Controllers.Users.Requests;
+using TaxBeacon.API.Controllers.Users.Responses;
+using TaxBeacon.Common.Enums;
+using TaxBeacon.Common.Services;
+using TaxBeacon.UserManagement.Models;
+using TaxBeacon.UserManagement.Services;
+
+namespace TaxBeacon.API.UnitTests.Controllers.Tenant;
+
+public class TenantsControllerTest
+{
+    private readonly Mock<IUserService> _userServiceMock;
+    private readonly TenantsController _controller;
+
+    public TenantsControllerTest()
+    {
+        _userServiceMock = new();
+        _controller = new TenantsController(_userServiceMock.Object)
+        {
+            ControllerContext = new ControllerContext()
+            {
+                HttpContext = new DefaultHttpContext
+                {
+                    User = new ClaimsPrincipal(new[] { new ClaimsIdentity(new[] { new Claim(Claims.TenantId, Guid.NewGuid().ToString()) }) })
+                }
+            }
+        };
+    }
+
+    [Fact]
+    public async Task GetUserList_ValidQuery_ReturnSuccessStatusCode()
+    {
+        // Arrange
+        var query = new GridifyQuery { Page = 1, PageSize = 25, OrderBy = "name desc", };
+        _userServiceMock.Setup(p => p.GetTenantsAsync(query, default)).ReturnsAsync(
+            new QueryablePaging<TenantDto>(0,
+                Enumerable.Empty<TenantDto>().AsQueryable()));
+
+        // Act
+        var actualResponse = await _controller.GetTenantList(query, default);
+
+        // Arrange
+        using (new AssertionScope())
+        {
+            var actualResult = actualResponse as OkObjectResult;
+            actualResponse.Should().NotBeNull();
+            actualResult.Should().NotBeNull();
+            actualResponse.Should().BeOfType<OkObjectResult>();
+            actualResult?.StatusCode.Should().Be(StatusCodes.Status200OK);
+            actualResult?.Value.Should().BeOfType<QueryablePaging<TenantResponse>>();
+        }
+    }
+
+    [Fact]
+    public async Task GetUserList_InvalidQuery_ReturnBadRequest()
+    {
+        // Arrange
+        var query = new GridifyQuery { Page = 1, PageSize = 25, OrderBy = "nonexistentfield desc", };
+        _userServiceMock.Setup(p => p.GetTenantsAsync(query, default)).ReturnsAsync(
+            new QueryablePaging<TenantDto>(0,
+                Enumerable.Empty<TenantDto>().AsQueryable()));
+
+        // Act
+        var actualResponse = await _controller.GetTenantList(query, default);
+
+        // Arrange
+        using (new AssertionScope())
+        {
+            var actualResult = actualResponse as BadRequestResult;
+            actualResponse.Should().NotBeNull();
+            actualResult.Should().NotBeNull();
+            actualResponse.Should().BeOfType<BadRequestResult>();
+            actualResult?.StatusCode.Should().Be(StatusCodes.Status400BadRequest);
+        }
+    }
+}
