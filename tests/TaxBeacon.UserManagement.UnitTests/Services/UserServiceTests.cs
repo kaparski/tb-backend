189eac38df95f1b852760d96cf48af4984eba981
Alexander Makar <alexandr.makar@kandasoft.com>
2023-04-05 19:33:50 +0300

    fixed unit tests

--- tests/TaxBeacon.UserManagement.UnitTests/Services/UserServiceTests.cs
+++ tests/TaxBeacon.UserManagement.UnitTests/Services/UserServiceTests.cs
@@ -157,9 +157,21 @@ public class UserServiceTests
     {
         // Arrange
         var users = TestData.TestUser.Generate(5);
+        var tenant = TestData.TestTenant.Generate();
+
+        await _dbContextMock.Tenants.AddAsync(tenant);
         await _dbContextMock.Users.AddRangeAsync(users);
+        await _dbContextMock.TenantUsers.AddRangeAsync(users.Select(u => new TenantUser
+        {
+            TenantId = tenant.Id,
+            UserId = u.Id
+        }));
         await _dbContextMock.SaveChangesAsync();
         var query = new GridifyQuery { Page = 1, PageSize = 10, OrderBy = "email asc" };
+
+        _currentUserServiceMock
+            .Setup(service => service.TenantId)
+            .Returns(tenant.Id);
 
         // Act
         var usersOneOf = await _userService.GetUsersAsync(query, default);
@@ -168,9 +180,9 @@ public class UserServiceTests
         usersOneOf.TryPickT0(out var pageOfUsers, out _);
         pageOfUsers.Should().NotBeNull();
         var listOfUsers = pageOfUsers.Query.ToList();
-        listOfUsers.Count.Should().Be(6);
+        listOfUsers.Count.Should().Be(5);
         listOfUsers.Select(x => x.Email).Should().BeInAscendingOrder();
-        pageOfUsers.Count.Should().Be(6);
+        pageOfUsers.Count.Should().Be(5);
     }
 
     [Fact]
@@ -178,9 +190,21 @@ public class UserServiceTests
     {
         // Arrange
         var users = TestData.TestUser.Generate(6);
+        var tenant = TestData.TestTenant.Generate();
+
+        await _dbContextMock.Tenants.AddAsync(tenant);
         await _dbContextMock.Users.AddRangeAsync(users);
+        await _dbContextMock.TenantUsers.AddRangeAsync(users.Select(u => new TenantUser
+        {
+            TenantId = tenant.Id,
+            UserId = u.Id
+        }));
         await _dbContextMock.SaveChangesAsync();
         var query = new GridifyQuery { Page = 1, PageSize = 4, OrderBy = "email desc" };
+
+        _currentUserServiceMock
+            .Setup(service => service.TenantId)
+            .Returns(tenant.Id);
 
         // Act
         var usersOneOf = await _userService.GetUsersAsync(query, default);
@@ -193,7 +217,7 @@ public class UserServiceTests
             var listOfUsers = pageOfUsers.Query.ToList();
             listOfUsers.Count.Should().Be(4);
             listOfUsers.Select(x => x.Email).Should().BeInDescendingOrder();
-            pageOfUsers.Count.Should().Be(7);
+            pageOfUsers.Count.Should().Be(6);
         }
     }
 
