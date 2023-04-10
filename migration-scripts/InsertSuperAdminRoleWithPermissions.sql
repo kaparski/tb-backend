1ca435497d1f0929a6d5d72007f1321858c3399f
Alexander Makar <alexandr.makar@kandasoft.com>
2023-04-10 13:19:15 +0300

    feat: Added a script to add the super admin role and the rights for this role

---
+++ migration-scripts/InsertSuperAdminRoleWithPermissions.sql
@@ -0,0 +1,18 @@
+﻿DECLARE @superAdminRoleName AS NVARCHAR(100) = N'Super admin';
+DECLARE @roleId AS UNIQUEIDENTIFIER = NEWID()
+
+IF NOT EXISTS(SELECT 1
+              FROM Roles
+              WHERE Name = @superAdminRoleName)
+    BEGIN
+        INSERT INTO Roles(Id, Name) VALUES (@roleId, @superAdminRoleName);
+    END;
+ELSE
+    BEGIN
+        SELECT @roleId = Id FROM Roles WHERE Name = @superAdminRoleName;
+    END;
+
+INSERT INTO RolePermissions(RoleId, PermissionId)
+SELECT @roleId, Id
+FROM Permissions p
+WHERE NOT EXISTS(SELECT 1 FROM RolePermissions WHERE RoleId = @roleId AND PermissionId = p.Id);
