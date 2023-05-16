b6ed4a458d89adc768129b71205b4e1087c5ea6d
Ilia Pulman <ilia.pulman@kandasoft.com>
2023-05-16 12:52:27 +0300

    fix: remove unneeded query check

--- src/TaxBeacon.UserManagement/Services/DepartmentService.cs
+++ src/TaxBeacon.UserManagement/Services/DepartmentService.cs
@@ -55,8 +55,7 @@ public class DepartmentService: IDepartmentService
     {
         var departments = await _context
             .Departments
-            .Where(d => d.TenantId == _currentUserService.TenantId
-                   && (d.Division == null || d.Division.TenantId == _currentUserService.TenantId))
+            .Where(d => d.TenantId == _currentUserService.TenantId)
             .Select(d => new DepartmentDto
             {
                 Id = d.Id,
