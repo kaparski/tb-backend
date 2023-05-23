53ad58850ef33939cce394bc87c87d40478f0c0b
Vlad Veko <vladislav.veko@kandasoft.com>
2023-05-23 11:45:25 +0300

    feat: dto refinements and jurisdiction name migration

---
+++ src/TaxBeacon.DAL/Migrations/20230522195335_UpdatedJurisdictionNameColumnForPrograms.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaxBeacon.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedJurisdictionNameColumnForPrograms : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "JurisdictionName",
+                table: "Programs",
+                type: "nvarchar(604)",
+                maxLength: 604,
+                nullable: true,
+                computedColumnSql: "TRIM(CASE WHEN [Jurisdiction] = 1 THEN 'Federal' WHEN [Jurisdiction] = 2 THEN [State] WHEN [Jurisdiction] = 3 THEN CONCAT_WS(', ',[State], [County], [City]) ELSE NULL END)",
+                stored: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(604)",
+                oldMaxLength: 604,
+                oldNullable: true,
+                oldComputedColumnSql: "TRIM(CASE WHEN [Jurisdiction] = 1 THEN 'Federal' WHEN [Jurisdiction] = 2 THEN [State] WHEN [Jurisdiction] = 3 THEN CONCAT([State], ', ', [County], ', ', [City]) ELSE NULL END)",
+                oldStored: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "JurisdictionName",
+                table: "Programs",
+                type: "nvarchar(604)",
+                maxLength: 604,
+                nullable: true,
+                computedColumnSql: "TRIM(CASE WHEN [Jurisdiction] = 1 THEN 'Federal' WHEN [Jurisdiction] = 2 THEN [State] WHEN [Jurisdiction] = 3 THEN CONCAT([State], ', ', [County], ', ', [City]) ELSE NULL END)",
+                stored: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(604)",
+                oldMaxLength: 604,
+                oldNullable: true,
+                oldComputedColumnSql: "TRIM(CASE WHEN [Jurisdiction] = 1 THEN 'Federal' WHEN [Jurisdiction] = 2 THEN [State] WHEN [Jurisdiction] = 3 THEN CONCAT_WS(', ',[State], [County], [City]) ELSE NULL END)",
+                oldStored: true);
+        }
+    }
+}
