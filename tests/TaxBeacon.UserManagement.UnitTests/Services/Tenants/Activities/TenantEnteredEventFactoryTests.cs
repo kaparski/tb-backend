2f3387bdeb049fc1e9e6110e7e6d4937fcad3053
Alexander Makar <alexandr.makar@kandasoft.com>
2023-06-05 10:26:10 +0300

    feat: added unit tests for tenant activities

---
+++ tests/TaxBeacon.UserManagement.UnitTests/Services/Tenants/Activities/TenantEnteredEventFactoryTests.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using FluentAssertions.Execution;
+using System.Text.Json;
+using TaxBeacon.UserManagement.Services.Tenants.Activities;
+using TaxBeacon.UserManagement.Services.Tenants.Activities.Models;
+
+namespace TaxBeacon.UserManagement.UnitTests.Services.Tenants.Activities;
+
+public sealed class TenantEnteredEventFactoryTests
+{
+    private readonly ITenantActivityFactory _activityFactory;
+
+    public TenantEnteredEventFactoryTests() => _activityFactory = new TenantEnteredEventFactory();
+
+    [Fact]
+    public void Create_heckMapping()
+    {
+        //Arrange
+        var date = DateTime.UtcNow;
+        var tenantAssignProgramsEvent = new TenantEnteredEvent(Guid.NewGuid(),
+            "Super Admin",
+            "Test",
+            date);
+
+        //Act
+        var result = _activityFactory.Create(JsonSerializer.Serialize(tenantAssignProgramsEvent));
+
+        //Arrange
+        using (new AssertionScope())
+        {
+            result.Date.Should().Be(date);
+            result.FullName.Should().Be("Test");
+            result.Message.Should().Be("User entered the tenant");
+        };
+    }
+}
---
+++ tests/TaxBeacon.UserManagement.UnitTests/Services/Tenants/Activities/TenantUnAssignProgramsEventFactoryTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using FluentAssertions.Execution;
+using System.Text.Json;
+using TaxBeacon.UserManagement.Services.Tenants.Activities;
+using TaxBeacon.UserManagement.Services.Tenants.Activities.Models;
+
+namespace TaxBeacon.UserManagement.UnitTests.Services.Tenants.Activities;
+
+public class TenantUnAssignProgramsEventFactoryTests
+{
+    private readonly ITenantActivityFactory _activityFactory;
+
+    public TenantUnAssignProgramsEventFactoryTests() => _activityFactory = new TenantUnAssignProgramsEventFactory();
+
+    [Fact]
+    public void Create_heckMapping()
+    {
+        //Arrange
+        var date = DateTime.UtcNow;
+        var tenantAssignProgramsEvent = new TenantUnassignProgramsEvent(Guid.NewGuid(),
+            "Super Admin",
+            "Test",
+            "Test",
+            date);
+
+        //Act
+        var result = _activityFactory.Create(JsonSerializer.Serialize(tenantAssignProgramsEvent));
+
+        //Arrange
+        using (new AssertionScope())
+        {
+            result.Date.Should().Be(date);
+            result.FullName.Should().Be("Test");
+            result.Message.Should().Be("Program(s) access removed from Tenant: Test");
+        };
+    }
+}
