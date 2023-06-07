1c92b4b839924104d9fc0f7dce4c24d27dc9527f
Vadim Bondaruk <vadim.bondaruk@kandasoft.com>
2023-06-07 23:28:27 +0300

    feat: added referral

---
+++ src/TaxBeacon.DAL/Entities/Accounts/Referral.cs
@@ -0,0 +1,21 @@
+﻿using TaxBeacon.Common.Accounts;
+
+namespace TaxBeacon.DAL.Entities.Accounts;
+public class Referral: BaseEntity
+{
+    public Guid TenantId { get; set; }
+
+    public Tenant Tenant { get; set; } = null!;
+
+    public Guid AccountId { get; set; }
+
+    public Account Account { get; set; } = null!;
+
+    public ReferralState State { get; set; }
+
+    public Guid? ManagerId { get; set; }
+
+    public User? Manager { get; set; }
+
+    public string? NaicsCode { get; set; }
+}
