fcebe8bb60df2b0371f52fd9fd5c10094647f62d
Ilia Pulman <ilia.pulman@kandasoft.com>
2023-06-08 14:13:36 +0300

    refactor: add some new and refactor existing scripts

---
+++ InsertAccountManagerRoleWithPermissions.sql
@@ -0,0 +1,92 @@
+﻿--- Insert Roles and Permission
+--- Assign Roles to all users in tenant
+--- Assign Permissions to Role
+
+DECLARE @accountManagerRolePermissions TABLE (NAME NVARCHAR(250));
+DECLARE @roleId UNIQUEIDENTIFIER = NEWID();
+DECLARE @tenantId AS UNIQUEIDENTIFIER;
+DECLARE @roleName AS NVARCHAR(100) = N'Account manager'
+
+BEGIN TRANSACTION [Tran1];
+BEGIN TRY
+	SELECT @tenantId = Id
+	FROM Tenants
+	WHERE Name ='CTI'
+
+	IF NOT EXISTS(SELECT Id FROM Roles WHERE Id = @roleId)
+	  BEGIN
+		INSERT INTO Roles(Id, Name, CreatedDateTimeUtc) VALUES (@roleId, @roleName, GETUTCDATE());
+	  END;
+	  ELSE
+		BEGIN
+			SELECT @roleId = Id
+			FROM ROLES r
+			JOIN TenantRoles tr ON tr.RoleId = r.Id
+			AND tr.TenantId = @tenantId AND r.Name = @roleName
+		END;
+
+	  IF NOT EXISTS(SELECT RoleId FROM TenantRoles WHERE RoleId = @roleId AND TenantId = @tenantId)
+	  BEGIN
+		INSERT INTO TenantRoles VALUES (@tenantId, @roleId);
+	  END;
+
+	  INSERT INTO TenantUsers
+		SELECT Id, @tenantId
+		FROM Users AS u
+		WHERE NOT EXISTS(SELECT UserId
+						 FROM TenantUsers
+						 WHERE UserId = u.Id
+						 AND TenantId = @tenantId);
+
+    INSERT INTO TenantUserRoles
+    SELECT TenantId, @roleId, UserId
+    FROM TenantUsers AS tu
+    WHERE NOT EXISTS(SELECT UserId
+                     FROM TenantUserRoles
+                     WHERE UserId = tu.UserId
+                     AND TenantId = tu.TenantId
+                     AND RoleId = @roleId);
+
+	INSERT INTO @accountManagerRolePermissions (Name)
+	VALUES ('Accounts.Read')
+
+	INSERT INTO Permissions (Id, Name, CreatedDateTimeUtc)
+	SELECT NEWID(),
+		   amrp.Name,
+		   GETUTCDATE()
+	FROM @accountManagerRolePermissions amrp
+	WHERE NOT EXISTS
+		(SELECT 1
+		 FROM Permissions
+		 WHERE Name = amrp.Name)
+
+	INSERT INTO TenantPermissions (TenantId, PermissionId)
+	SELECT @tenantId,
+		   Id
+	FROM Permissions p
+	WHERE NOT EXISTS
+		(SELECT 1
+		 FROM TenantPermissions
+		 WHERE TenantId = @tenantId
+		   AND PermissionId = p.Id)
+
+	INSERT INTO TenantRolePermissions (TenantId, RoleId, PermissionId)
+	SELECT @tenantId,
+		   @roleId,
+		   Id
+	FROM Permissions p
+	WHERE p.Name in
+		(SELECT Name
+		 FROM @accountManagerRolePermissions)
+	  AND NOT EXISTS
+		(SELECT 1
+		 FROM TenantRolePermissions
+		 WHERE TenantId = @tenantId
+		   AND RoleId = @roleId
+		   AND PermissionId = p.Id)
+
+		   COMMIT TRANSACTION [Tran1];
+END TRY
+BEGIN CATCH
+  ROLLBACK TRANSACTION [Tran1]
+END CATCH
--- migration-scripts/InsertSuperAdminRoleWithPermissions.sql
+++ migration-scripts/InsertSuperAdminRoleWithPermissions.sql
@@ -1,19 +1,64 @@
-﻿DECLARE @superAdminRoleName AS NVARCHAR(100) = N'Super admin';
+﻿DECLARE @rolePermissions TABLE (NAME NVARCHAR(250));
 DECLARE @roleType AS INT = 1;
-DECLARE @roleId AS UNIQUEIDENTIFIER = NEWID()
+DECLARE @roleId AS UNIQUEIDENTIFIER = NEWID();
+DECLARE @roleName AS NVARCHAR(100) = N'Super admin';
 
 IF NOT EXISTS(SELECT 1
               FROM Roles
-              WHERE Name = @superAdminRoleName)
+              WHERE Name = @roleName)
     BEGIN
-        INSERT INTO Roles(Id, Name, Type) VALUES (@roleId, @superAdminRoleName, @roleType);
+        INSERT INTO Roles(Id, Name, Type) VALUES (@roleId, @roleName, @roleType);
     END;
 ELSE
     BEGIN
-        SELECT @roleId = Id FROM Roles WHERE Name = @superAdminRoleName;
+        SELECT @roleId = Id FROM Roles WHERE Name = @roleName;
     END;
+
+INSERT INTO @rolePermissions
+    (Name)
+VALUES ('Departments.Read'),
+       ('Departments.ReadWrite'),
+       ('Departments.ReadExport'),
+       ('Divisions.Activation'),
+       ('Divisions.Read'),
+       ('Divisions.ReadWrite'),
+       ('Divisions.ReadExport'),
+       ('JobTitles.Read'),
+       ('JobTitles.ReadWrite'),
+       ('JobTitles.ReadExport'),
+       ('Programs.Read'),
+       ('Programs.ReadWrite'),
+       ('Programs.ReadExport'),
+       ('Roles.Read'),
+       ('Roles.ReadWrite'),
+       ('ServiceAreas.Read'),
+       ('ServiceAreas.ReadWrite'),
+       ('ServiceAreas.ReadExport'),
+       ('TableFilters.Read'),
+       ('TableFilters.ReadWrite'),
+       ('Teams.Read'),
+       ('Teams.ReadWrite'),
+       ('Teams.ReadExport'),
+       ('Tenants.Read'),
+       ('Tenants.ReadWrite'),
+       ('Tenants.ReadExport'),
+       ('Users.Read'),
+       ('Users.ReadWrite'),
+       ('Users.ReadExport')
+
+INSERT INTO Permissions (Id, Name, CreatedDateTimeUtc)
+SELECT NEWID(),
+       srp.Name,
+       GETUTCDATE()
+FROM @rolePermissions srp
+WHERE NOT EXISTS
+    (SELECT 1
+     FROM Permissions
+     WHERE Name = srp.Name)
 
 INSERT INTO RolePermissions(RoleId, PermissionId)
 SELECT @roleId, Id
 FROM Permissions p
-WHERE NOT EXISTS(SELECT 1 FROM RolePermissions WHERE RoleId = @roleId AND PermissionId = p.Id)
+WHERE p.Name in
+    (SELECT Name FROM @rolePermissions)
+    AND NOT EXISTS(SELECT 1 FROM RolePermissions WHERE RoleId = @roleId AND PermissionId = p.Id);
