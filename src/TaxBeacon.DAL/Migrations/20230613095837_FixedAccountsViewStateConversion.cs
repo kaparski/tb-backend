d42272cf66a694065cfe11dfdf946cd9e03ac4d3
Alexander Makar <alexandr.makar@kandasoft.com>
2023-06-13 13:17:43 +0300

    fix: fixed accounts view state conversion

---
+++ src/TaxBeacon.DAL/Migrations/20230613095837_FixedAccountsViewStateConversion.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaxBeacon.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixedAccountsViewStateConversion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
