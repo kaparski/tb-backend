e6f47907b80e97662a5435b2048ce0f98894c97c
Vlad Veko <vladislav.veko@kandasoft.com>
2023-07-10 15:15:21 +0300

    fix: converted PhoneType to enum

--- src/TaxBeacon.Common/Enums/Accounts/PhoneType.cs
+++ src/TaxBeacon.Common/Enums/Accounts/PhoneType.cs
@@ -1,14 +1,8 @@
-using Ardalis.SmartEnum;
-
 namespace TaxBeacon.Common.Enums.Accounts;
 
-public class PhoneType: SmartEnum<PhoneType>
+public enum PhoneType
 {
-    public static readonly PhoneType Office = new("Office", 0);
-    public static readonly PhoneType Mobile = new("Mobile", 1);
-    public static readonly PhoneType Fax = new("Fax", 2);
-
-    public PhoneType(string name, int value) : base(name, value)
-    {
-    }
+    Office,
+    Mobile,
+    Fax
 }
